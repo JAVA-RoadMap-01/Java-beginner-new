4be8796ec63fa50a0270e7d03732cfff71564cd8
Aradhana-02 <aradhana.0281@gmail.com>
2025-10-05 23:37:26 +0530

    Day3 Task

---
+++ src/main/java/org/example/StringsJava.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class StringsJava
+{
+    public static void main(String[] args){
+        String word1 = "Hello";
+        String word2 = "Hello";
+
+        System.out.println(word1 == word2);
+        System.out.println(word1.equals(word2));
+    }
+}
