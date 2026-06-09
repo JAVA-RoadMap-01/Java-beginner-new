16ca37e2febd8f97eee08a982570ae7c643d0a79
Aradhana-02 <aradhana.0281@gmail.com>
2026-06-09 21:58:51 +0530

    Stream API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,40 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+//STREAM API
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        List<Integer> nums = Arrays.asList(4,5,6,7);
+
+        //Stream<Integer> data = nums.stream().map(n -> n*2);
+        //Predicate<Integer> test = n -> n%2==1;
+
+//        nums.stream()
+//                .filter(test)
+//                .map(n -> n*2)
+//                .forEach(n-> System.out.println(n));
+
+        nums.parallelStream()
+                .forEach(n -> System.out.println(n));
+
+        //other way :  nums.stream().parallel();
+       // Stream<Integer> mappedData = data.map(n -> n*2);
+        //mappedData.forEach(n -> System.out.println(n));
+        //Streams can be only used once.
+//        data.forEach(n -> System.out.println(n));
+//        data.forEach(System.out::println);
+
+        //Traditional way
+//        for(int n : nums){
+//            System.out.println(n*2);
+//        }
+
     }
 }
